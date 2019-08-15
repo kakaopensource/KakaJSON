c39ee2a52d1a2222c34467ec97ab5f365540b21e
coder_YT <boluoboluomi@aliyun.com>
2019-08-15 16:40:20 +0800

    FIX:重复提交

--- KakaJSON/Metadata/Descriptor/FieldDescriptor.swift
+++ KakaJSON/Metadata/Descriptor/FieldDescriptor.swift
@@ -39,7 +39,7 @@ struct FieldRecord {
     let flags: Int32
     var _mangledTypeName: RelativeDirectPointer<UInt8>
     var _fieldName: RelativeDirectPointer<UInt8>
-    //todo : vesion:1.0.2 修改 返回值问题 
+    //todo : vesion:1.0.2 修改 返回值问题
     var isVar: Bool { get { return (flags & 0x2) == 0x2 } }
     mutating func fieldName() -> String { String(cString: _fieldName.advanced()) }
     mutating func mangledTypeName() -> String { String(cString: _mangledTypeName.advanced()) }
