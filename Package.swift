9e1cd4c3d0c231ac29446c95439506ac7b60bb72
Luo Xiu <luoxiustm@gmail.com>
2019-09-05 15:35:26 +0800

    add deployment group for package manager configurations

--- Package.swift
+++ Package.swift
@@ -5,19 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "KakaJSON",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "KakaJSON",
             targets: ["KakaJSON"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "KakaJSON",
             dependencies: []),
